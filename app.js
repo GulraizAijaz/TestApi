fef8e49ae9b8c791cc6073b2b4b6598c631ea79b
Gulraiz <gulraizmuhammad013@gmail.com>
2024-09-19 19:03:06 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser")
+const cors = require('cors');
+
+
+// app
+const app = express();
+app.use(express.json());
+app.use(cors()); // Enable all CORS requests
+
+
+
+app.use(bodyParser.json());
+
+
+app.get("/",(req,res)=>{
+    res.send("hellooo from serverr")
+})
+
+
+
+const port = process.env.PORT  || 3000;
+console.log(port)
+app.listen(port,()=>{
+    console.log("server is runnnning on this port"+ port)
+})
